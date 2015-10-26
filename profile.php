824bcb04ac5f3198b4f7738fc98f428075d13182
Akmal <akmal.thotawatte@gmail.com>
2015-10-26 22:26:52 +1100

    Corrected links in help page

--- profile.php
+++ profile.php
@@ -6,129 +6,129 @@
  * Time: 12:50 PM
  */
 
-$type = $_POST['type'];
+    $type = $_POST['type'];
 
 
-if($type == "1"){
-    contactdetails();
-}else if($type == "2"){
-    courseinfo();
-}
-
-function contactdetails(){
-    $firstname = $_POST['first'];
-    $lastname = $_POST['last'];
-    $username = $_POST['user'];
-    $password = $_POST['pass'];
-    $pdesc = $_POST['pdesc'];
-
-    $result = "Your changes were saved successfully, ";
-    $sql = "UPDATE user SET";
-    if($firstname != ""){
-        $result = $result. " First name - $firstname";
-        $sql = $sql." firstname = $firstname";
+    if($type == "1"){
+        contactdetails();
+    }else if($type == "2"){
+        courseinfo();
     }
-    if($lastname != ""){
-        $result = $result.", Last name - $lastname";
+
+    function contactdetails(){
+        $firstname = $_POST['first'];
+        $lastname = $_POST['last'];
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+        $pdesc = $_POST['pdesc'];
+
+        $result = "Your changes were saved successfully, ";
+        $sql = "UPDATE user SET";
         if($firstname != ""){
-            $sql = $sql.", surname = $lastname";
-        }else{
-            $sql = $sql."surname = $lastname";
+            $result = $result. " First name - $firstname";
+            $sql = $sql." firstname = $firstname";
         }
-
-    }
-
-    if($username != ""){
-        $result = $result. ", User name - $username";
         if($lastname != ""){
-        //    $sql = $sql.", username = $username";
-        }else{
-        //    $sql = $sql."username = $username";
-        }
-    }
+            $result = $result.", Last name - $lastname";
+            if($firstname != ""){
+                $sql = $sql.", surname = $lastname";
+            }else{
+                $sql = $sql."surname = $lastname";
+            }
 
-    if($password != ""){
-        $result = $result. ", Password - $password";
+        }
+
         if($username != ""){
-            $sql = $sql.", password = $password";
-        }else{
-            $sql = $sql."password = $password";
+            $result = $result. ", User name - $username";
+            if($lastname != ""){
+               $sql = $sql.", username = $username";
+            }else{
+               $sql = $sql."username = $username";
+            }
         }
-    }
 
-    if($pdesc != ""){
-        $result = $result. ", Description - $pdesc";
         if($password != ""){
-            $sql = $sql.", personal = $pdesc";
-        }else{
-            $sql = $sql."personal = $pdesc";
+            $result = $result. ", Password - $password";
+            if($username != ""){
+                $sql = $sql.", password = $password";
+            }else{
+                $sql = $sql."password = $password";
+            }
         }
-    }
 
-
-    $sql = $sql. " WHERE username = 'testu'";
-
-    //echo $sql;
-
-    $conn = @mysqli_connect("fdb14.biz.nf", "1971863_student","swinedc123","1971863_student");
-    ​
-    if(!$conn){
-        die("Something went wrong, please try again later");
-    }else{
-
-        $resultq = $conn->query($sql);
-        if($resultq == TRUE){
-            echo $result;
-        }else{
-            echo "Something went wrong, please try again later";
+        if($pdesc != ""){
+            $result = $result. ", Description - $pdesc";
+            if($password != ""){
+                $sql = $sql.", personal = $pdesc";
+            }else{
+                $sql = $sql."personal = $pdesc";
+            }
         }
+
+
+        $sql = $sql. " WHERE username = 'testu'";
+
+        //echo $sql;
+
+        $conn = @mysqli_connect("fdb14.biz.nf", "1971863_student","swinedc123","1971863_student");
+
+        if(!$conn){
+           die("Something went wrong, please try again later");
+        }else{
+
+            $resultq = $conn->query($sql);
+            if($resultq == TRUE){
+                echo $result;
+            }else{
+                echo "Something went wrong, please try again later";
+            }
+        }
+
     }
 
-}
 
+    function courseinfo(){
 
-function courseinfo(){
+        $coname = $_POST['pass'];
+        $year = $_POST['pdesc'];
+        $result = "Your changes were saved successfully, ";
 
-    $coname = $_POST['pass'];
-    $year = $_POST['pdesc'];
-    $result = "Your changes were saved successfully ";
-
-    $sql = "UPDATE user SET";
-    if($coname != ""){
-        $result = $result. ", Course name - $coname";
-        $sql = $sql." course = $coname";
-    }
-    if($year != ""){
-        $result = $result. ", Year started - $year";
-        $year = $year. " 00:00:00";
+        $sql = "UPDATE user SET";
+        if($coname != ""){
+            $result = $result. ", Course name - $coname";
+            $sql = $sql." course = $coname";
+        }
         if($year != ""){
-            $sql = $sql.", year = $year";
+            $result = $result. ", Year started - $year";
+            $year = $year. " 00:00:00";
+            if($year != ""){
+                $sql = $sql.", yearstarted = $year";
+            }else{
+                $sql = $sql."yearstarted = $year";
+            }
+
+        }
+
+
+
+
+        $sql = $sql. " WHERE username = 'testu'";
+
+        //echo $sql;
+
+        $conn = @mysqli_connect("fdb14.biz.nf", "1971863_student","swinedc123","1971863_student");
+        ​
+        if(!$conn){
+            die("Something went wrong, please try again later");
         }else{
-            $sql = $sql."year = $year";
+
+            $result = $conn->query($sql);
+            if($result == TRUE){
+                echo "Update successful";
+            }else{
+                echo "Something went wrong, please try again later";
+            }
         }
 
     }
-
-
-
-
-    $sql = $sql. " WHERE username = 'testu'";
-
-    //echo $sql;
-
-    $conn = @mysqli_connect("fdb14.biz.nf", "1971863_student","swinedc123","1971863_student");
-    ​
-    if(!$conn){
-        die("Something went wrong, please try again later");
-    }else{
-
-        $result = $conn->query($sql);
-        if($result == TRUE){
-            echo "Update successful";
-        }else{
-            echo "Something went wrong, please try again later";
-        }
-    }
-
-}
 ?>
